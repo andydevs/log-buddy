5c5847f347f12adaa418e8264e0da9f56e77f677
Anshul Kharbanda <akanshul97@gmail.com>
2025-03-21 04:26:42 +0000

    Add more test cases for log_function

--- test/test_log_function.py
+++ test/test_log_function.py
@@ -2,15 +2,8 @@ import pytest
 import log_buddy
 
 
-def test_log_function_debug_log_default_args_function_args(caplog: pytest.LogCaptureFixture):
-    """
-    Test log function with default args and just using args
-    in function
-    """
-    caplog.set_level(log_buddy.DEBUG)
-    log = log_buddy.getLogger("test")
-
-    @log_buddy.log_function(log)
+@pytest.fixture
+def my_function_data():
     def my_function(log: log_buddy.Logger, a: int, b: int, z: int = 4) -> int:
         """
         My custom function with it's own docstring
@@ -18,9 +11,64 @@ def test_log_function_debug_log_default_args_function_args(caplog: pytest.LogCap
         log.info("Internal log... got a = %s, b = %s, and z = %s", a, b, z)
         return (a - b) * z
 
-    result = my_function(2, 3, 1)
+    return my_function, "my_function"
 
-    assert all(r.name == "test.my_function" for r in caplog.records)
+
+def test_log_function_default_args_debug_log_function_args(my_function_data, caplog):
+    """
+    Test log function with default args and just using args in function
+    """
+    caplog.set_level(log_buddy.DEBUG)
+    log = log_buddy.getLogger("test")
+    my_function, my_function_name = my_function_data
+    my_function = log_buddy.log_function(log)(my_function)
+    result = my_function(2, 3, 1)
+    assert all(r.name == f"test.{my_function_name}" for r in caplog.records)
     assert "Called with args (2, 3, 1) and kwargs {}" in caplog.messages
     assert f"Returned {result}" in caplog.messages
     assert "Internal log... got a = 2, b = 3, and z = 1" in caplog.messages
+
+
+def test_log_function_default_args_debug_log_function_args_and_kwargs(my_function_data, caplog):
+    """
+    Test log function with default args and just using args in function
+    """
+    caplog.set_level(log_buddy.DEBUG)
+    log = log_buddy.getLogger("test")
+    my_function, my_function_name = my_function_data
+    my_function = log_buddy.log_function(log)(my_function)
+    result = my_function(2, b=1, z=8)
+    assert all(r.name == f"test.{my_function_name}" for r in caplog.records)
+    assert "Called with args (2,) and kwargs {'b': 1, 'z': 8}" in caplog.messages
+    assert f"Returned {result}" in caplog.messages
+    assert "Internal log... got a = 2, b = 1, and z = 8" in caplog.messages
+
+
+def test_log_function_default_args_info_log_function_args(my_function_data, caplog):
+    """
+    Test log function with default args and just using args in function
+    """
+    caplog.set_level(log_buddy.INFO)
+    log = log_buddy.getLogger("test")
+    my_function, my_function_name = my_function_data
+    my_function = log_buddy.log_function(log)(my_function)
+    result = my_function(2, 3, 1)
+    assert all(r.name == f"test.{my_function_name}" for r in caplog.records)
+    assert "Called with args (2, 3, 1) and kwargs {}" not in caplog.messages
+    assert f"Returned {result}" not in caplog.messages
+    assert "Internal log... got a = 2, b = 3, and z = 1" in caplog.messages
+
+
+def test_log_function_default_args_info_log_function_args_and_kwargs(my_function_data, caplog):
+    """
+    Test log function with default args and just using args in function
+    """
+    caplog.set_level(log_buddy.INFO)
+    log = log_buddy.getLogger("test")
+    my_function, my_function_name = my_function_data
+    my_function = log_buddy.log_function(log)(my_function)
+    result = my_function(2, b=1, z=8)
+    assert all(r.name == f"test.{my_function_name}" for r in caplog.records)
+    assert "Called with args (2,) and kwargs {'b': 1, 'z': 8}" not in caplog.messages
+    assert f"Returned {result}" not in caplog.messages
+    assert "Internal log... got a = 2, b = 1, and z = 8" in caplog.messages
